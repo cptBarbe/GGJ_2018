743a910a60c1ad04cd506238d26a113ab392eab0
root <root@DESKTOP-2LNFKMJ.localdomain>
2018-01-27 11:51:39 -0800

    Adding files, making menu

---
+++ Assets/ChargerScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChargerScene : MonoBehaviour {
+
+    public void ChargerMaScene (string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
